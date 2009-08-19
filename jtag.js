03d9e54bfc7cb5f287025dc2bb763f9b7b799535
Tom Wilson <tom@jackhq.com>
2009-08-18 23:46:10 -0400

    removed global string and refactored jTag

--- jtag.js
+++ jtag.js
@@ -1,5 +1,5 @@
 /*
-* Tag JavaScript Library v0.3.0
+* Tag JavaScript Library v0.5.0
 * http://tag.jackhq.com
 *
 * Copyright (c) 2009 Jack Russell Software Company, LLC
@@ -7,43 +7,15 @@
 * http://github.com/twilson63/jtag/License
 *
 * Date: 2009-06-24 23:43:21 -0500 (Wed, 24 June 2009)
-* Revision: 00003
+* Revision: 0010
 */
 
 
 // Common Tags
 var 
-
-h1 = "<h1>?</h1>",
-h2 = "<h2>?</h2>",
-h3 = "<h3>?</h3>",
-h4 = "<h4>?</h4>",
-h5 = "<h5>?</h5>",
-h6 = "<h6>?</h6>",
-p = "<p>?</p>",
-br = "<br />",
-hr = "<hr />",
-ul = "<ul>?</ul>",
-li = "<li>?</li>",
-a = "<a>?</a>",
-div = "<div>?</div>",
-span = "<span>?</span>",
-input = "<input>?</input>",
-textarea = "<textarea>?</textarea>",
-select = "<select>?</select>",
-option = "<option>?</option>",
-label = "<label>?</label>",
-form = "<form>?</form>",
-table = "<table>?</table>",
-thead = "<thead>?</thead>",
-tr = "<tr>?</tr>",
-tbody = "<tbody>?</tbody>",
-td = "<td>?</td>",
-th = "<th>?</th>",
-form = "<form>?</form>",
-fieldset = "<fieldset>?</fieldset>",
-legend = "<legend>?</legend>",
-space = " ";
+_hr = "<hr />",
+_br = "<br />",
+_space = " ";
 
 (function(){
   var 
@@ -51,24 +23,29 @@ space = " ";
     undefined,
     _jTag = window.jTag,
 
-    jTag = window.jTag = function( tag, text, args ) {
-      return jTag.fn.init( tag, text, args )
+    jTag = window.jTag = function( tag, text, attributes ) {
+      return jTag.fn.init( tag, text, attributes )
     };
 
   jTag.fn = jTag.prototype = {
-    init: function( tag, text, args ) {
+    init: function( tag, text, attributes ) {
       if(typeof(tag) != "string") {
         throw "tag must be of type string not:" + typeof(tag) + " text is:" + text;
       }
-      if (tag.match(/\?/) == null) {
-        throw "tag must have ? in expression: " + tag;
-      } 
       
-      if (args) {
-        val = tag.replace(/>\?/, " " + args + ">\?")
+      if (tag.match(/h[1..6]|p|div|span|ul|li|input|form|fieldset|legend|label|a|textarea|select|option|br|hr|table|tbody|thead|td|th|tr|big/i) == null) {
+        throw "tag must be a valid html symbol: not " + tag;
+      }
+       
+      
+      if (attributes) {
+        if(typeof(attributes) == "string") {
+          attributes = [attributes];
+        }
+        val = "<x>?</x>".replace(/x/g,tag).replace(/>\?/, " " + attributes.join(' ') + ">\?");
         val = val.replace(/\?/, text);
       } else {
-        val = tag.replace(/\?/, text);
+        val = "<x>?</x>".replace(/x/g,tag).replace(/\?/, text);
       }
       return val;
     }
@@ -82,20 +59,20 @@ space = " ";
 
     _jInput = window.jInput,
     jInput = window.jInput = function( name, value, type, args ) {
-    	return jTag("<input>?</input>", "", 
+    	return jTag("input", "", 
     	  [ 
-    	  jAttribute("id", type == "radio" ? [name.split('[').join('_').replace(/\]/g,''),value].join('_') : name.split('[').join('_').replace(/\]/g,'')),
-    	  jAttribute("name", name), 
-    	  jAttribute("value", value || ""), 
-    	  jAttribute("type", type),
+    	  jAt("id", type == "radio" ? [name.split('[').join('_').replace(/\]/g,''),value].join('_') : name.split('[').join('_').replace(/\]/g,'')),
+    	  jAt("name", name), 
+    	  jAt("value", value || ""), 
+    	  jAt("type", type),
     	  args || ""
-    	  ].join(' '));
+    	  ]);
 
     },
     
     _jLink = window.jLink,
     jLink = window.jLink = function( name, value, href, args ) {
-      return jTag("<a>?</a>", value, [jAttribute("id", name), jAttribute("href", href || "#"), args || ""].join(' '));
+      return jTag("a", value, [jAt("id", name), jAt("href", href || "#"), args || ""]);
     },
     
     _jSubmit = window.jSubmit,
@@ -113,7 +90,7 @@ space = " ";
     },
     _jLabel = window.jLabel,
     jLabel = window.jLabel = function( name, value, args ) {
-      return jTag("<label>?</label>", value, jAt("for", name.split('[').join('_').replace(/\]/g,'')) + space + (args || ""));
+      return jTag("label", value, jAt("for", name.split('[').join('_').replace(/\]/g,'')) + _space + (args || ""));
     };
     _jHidden = window.jHidden,
     jHidden = window.jHidden = function( name, value ) {
@@ -125,7 +102,7 @@ space = " ";
     };
     _jArea = window.jArea,
     jArea = window.jArea = function( name, value, attr ) {
-      return jTag("<textarea>?</textarea>", value || "", [attr || "", jAt("id", name.split('[').join('_').replace(/\]/g,'')), jAt("name", name)].join(' ') );
+      return jTag("textarea", value || "", [attr || "", jAt("id", name.split('[').join('_').replace(/\]/g,'')), jAt("name", name)].join(' ') );
     };
     _jCheck = window.jCheck,
     jCheck = window.jCheck = function( name, value, args ) {
@@ -137,12 +114,12 @@ space = " ";
       options = "";
       for(i = 0; i < values.length; i++) {
         if(values[i].id && values[i].name) {
-            options += jTag(option, values[i].name, jAttribute("value", values[i].id));
+            options += jTag(option, values[i].name, jAt("value", values[i].id));
         } else {
-            options += jTag(option, values[i], jAttribute("value", values[i]));
+            options += jTag(option, values[i], jAt("value", values[i]));
         }
       }
-      return jTag("<select>?</select>", options, [args || "", jAt("id", name.split('[').join('_').replace(/\]/g,'')), jAt("name",name)].join(' '));
+      return jTag("select", options, [args || "", jAt("id", name.split('[').join('_').replace(/\]/g,'')), jAt("name",name)]);
     };
 
     _jDiv = window.jDiv,
@@ -169,32 +146,41 @@ space = " ";
         }
       }
       
-      classes = jAttribute("class", classes.join(' '));
-      id = jAttribute("id", id);
+      classes = jAt("class", classes.join(' '));
+      id = jAt("id", id);
       
-      return jTag(div, innerHtml, [attributes || "", classes, id].join(' '));
+      return jTag("div", innerHtml, [attributes || "", classes, id]);
     },
     _jTable = window.jTable, 
     jTable = window.jTable = function (headers, data, args) {
       header_markup = "";
       for(i = 0; i < headers.length; i++) {
-        header_markup += jTag(th, headers[i]);
+        header_markup += jTag("th", headers[i]);
       }
       rows = "";
       for(i = 0; i < data.length; i++) {
-        rows += jTag(tr, function () {
+        rows += jTag("tr", function () {
           columns = "";
           for(y = 0; y < data[i].length; y++) {
-            columns += jTag(td, data[i][y]);
+            columns += jTag("td", data[i][y]);
           }
           return columns;
         });
       }
-      return jTag(table, 
-        jTag(thead, jTag(tr, header_markup)) +
-        jTag(tbody, rows)
-        , args || "")
+      return jTag("table", 
+        jTag("thead", jTag("tr", header_markup)) +
+        jTag("tbody", rows)
+        , args || "");
       
+    },
+    _jList = window.jList,
+    jList = window.jList = function (items, attributes) {
+      var line_items = "";
+      for(i = 0; i < items.length; i++) {
+        line_items += jTag("li", items[i]);
+      }
+      
+      return jTag("ul", line_items, attributes || "");
     }
     
 })();
